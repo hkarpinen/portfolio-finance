6d6232838f96a4943c5fc4be72a5d8eaa18b170e
Hank <85519781+hkarpinen@users.noreply.github.com>
2026-05-21 17:12:03 -0700

    prefix queue names so they do not compete.

--- src/Infrastructure/InfrastructureServiceExtensions.cs
+++ src/Infrastructure/InfrastructureServiceExtensions.cs
@@ -30,7 +30,7 @@ public static class InfrastructureServiceExtensions
         var rabbitConfig = configuration.GetSection("RabbitMq");
         services.AddMassTransit(x =>
         {
-            x.SetKebabCaseEndpointNameFormatter();
+            x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("finance", false));
 
             x.AddConsumer<UserRegisteredConsumer>();
             x.AddConsumer<UserProfileUpdatedConsumer>();
